7524a1166925531ebd3e0e0e6e494c2d0c56720d
Jignesh Sheth <jigneshsheth@Jigs-Mac-Pro.local>
2020-04-30 15:02:47 -0500

    update

--- GuessDistance/src/main/java/com/example/GuessDistance.java
+++ GuessDistance/src/main/java/com/example/GuessDistance.java
@@ -17,13 +17,14 @@ public class GuessDistance {
 
     /**
      * Validates is your guess is within allowable threshold (+- 10%)
-     * @param guess value user guessed
-     * @param actual actual distance value
+     * 
+     * @param guess     value user guessed
+     * @param actual    actual distance value
      * @param threshold threshold allowed to consider close enough
      * @return true or false if you guessed it close enough
      */
     public boolean checkGuess(double guess, double actual, double threshold) {
-        if (guess/actual * 100 >= (100-threshold) && guess/actual * 100 <= (100+threshold)) {
+        if (guess / actual * 100 >= (100 - threshold) && guess / actual * 100 <= (100 + threshold)) {
             return true;
         }
         return false;
@@ -31,6 +32,7 @@ public class GuessDistance {
 
     /**
      * Picks two random cities from the pre-populated US cities list
+     * 
      * @return two cities to guess distance.
      */
     public City[] getRandomTwoCities() {
@@ -47,8 +49,9 @@ public class GuessDistance {
 
     /**
      * Calculates disctance between two cities
+     * 
      * @param start starting city
-     * @param end destination city
+     * @param end   destination city
      * @return distance
      */
     public double calculateDistance(City start, City end) {
@@ -57,6 +60,7 @@ public class GuessDistance {
 
     /**
      * Main class to run the program
+     * 
      * @param args accepts arguments as String
      */
     public static void main(String[] args) {
@@ -68,43 +72,44 @@ public class GuessDistance {
         response = scan.next(); // read user input
         yes = response.matches("(y|Y|yes|Yes|YES|Yeah|yeah|YEAH|yep|Yep|YEP|ok|OK)");
         GuessDistance gDistance = new GuessDistance();
-        while(yes) {
-        // if (yes) {
+        while (yes) {
             try {
                 City[] gCity = gDistance.getRandomTwoCities();
-                System.out.println("Starting city: "+gCity[0].name);
-                System.out.println("Destination city: "+gCity[1].name);
+                System.out.println("Starting city: " + gCity[0].name);
+                System.out.println("Destination city: " + gCity[1].name);
                 System.out.print("How far do you think these cities are from each other in miles? ");
                 double distance = scan.nextDouble();
                 double actual_distance = gDistance.calculateDistance(gCity[0], gCity[1]);
-                if (gDistance.checkGuess(distance,actual_distance,10)) {
-                    System.out.println("\nAwsome job! You guessed it within +- 10%: "+Math.floor(distance/actual_distance*100));
-                    System.out.println("\nActual distance between cities: "+String.format("%.2f", actual_distance));
+                if (gDistance.checkGuess(distance, actual_distance, 10)) {
+                    System.out.println("\nAwsome job! You guessed it within +- 10%: "
+                            + Math.floor(distance / actual_distance * 100));
+                    System.out.println("Actual distance between cities: " + String.format("%.2f", actual_distance));
                 } else {
                     System.out.println("\nGood luck next time! You were off by more than 10% ...");
                     do {
-                        System.out.print("\nDo you want to try to guess again?? (y/n) ");
+                        System.out.print("Do you want to try to guess again?? (y/n) ");
                         response = scan.next(); // read user input
                         yes = response.matches("(y|Y|yes|Yes|YES|Yeah|yeah|YEAH|yep|Yep|YEP|ok|OK)");
-                        if(yes) {
-                            System.out.print("\nTry your best guess!! ");
+                        if (yes) {
+                            System.out.print("Try your best guess!! ");
                             distance = scan.nextDouble();
-                            if (gDistance.checkGuess(distance,actual_distance,10)) {
-                                System.out.println("\nAwsome job! You guessed it within +- 10%: "+Math.floor(distance/actual_distance*100));
+                            if (gDistance.checkGuess(distance, actual_distance, 10)) {
+                                System.out.println("\nAwsome job! You guessed it within +- 10%: "
+                                        + Math.floor(distance / actual_distance * 100));
                                 yes = false;
                             } else {
                                 System.out.println("\nNot bad ... Good luck next time!");
-                                System.out.println("Your guess is too "+(distance > actual_distance ? "high" : "low"));
+                                System.out
+                                        .println("Your guess is too " + (distance > actual_distance ? "high" : "low"));
                             }
                         }
                     } while (yes);
-                    System.out.println("\nActual distance between cities: "+String.format("%.2f", actual_distance));
+                    System.out.println("Actual distance between cities: " + String.format("%.2f", actual_distance));
                 }
-                // System.out.println(cities);
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
-            System.out.print("\nDo you want to try to guess again?? (y/n) ");
+            System.out.print("\nDo you want to play again?? (y/n) ");
             response = scan.next(); // read user input
             yes = response.matches("(y|Y|yes|Yes|YES|Yeah|yeah|YEAH|yep|Yep|YEP|ok|OK)");
         }
