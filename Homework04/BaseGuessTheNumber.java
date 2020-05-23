61caaa46dd2b1a97106cdb89f5f17d7277918d7e
Jig Sheth <jsheth@pivotal.io>
2020-05-22 19:29:02 -0500

    update

--- Homework04/BaseGuessTheNumber.java
+++ Homework04/BaseGuessTheNumber.java
@@ -2,6 +2,9 @@ public class BaseGuessTheNumber {
 
     private int number2Guess;
     private int[] range;
+    public static final int MIN = 0;
+    public static final int MAX = 1;
+
 
     /**
      * Constructor to define minimum and maximum range of numbers for user to guess.
@@ -9,7 +12,7 @@ public class BaseGuessTheNumber {
      * @param max upper bound of range
      */
     public BaseGuessTheNumber(int min, int max) {
-        range = new int[]{min, max};
+        setRange(min, max);
         newNumber2Guess();
     }
 
@@ -31,11 +34,18 @@ public class BaseGuessTheNumber {
     }
 
     /**
+     * return range value
+     * @return int[] of range value
+     */
+    public int[] getRange() {
+        return range;
+    }
+    /**
      * Generate new number to guess from given range of numbers.
      * @return int number to guess
      */
     public int newNumber2Guess() {
-        number2Guess = range[0] + (int)(Math.random() * ((range[1] - range[0])+1));
+        number2Guess = range[MIN] + (int)(Math.random() * ((range[MAX] - range[MIN])+1));
         return number2Guess;
     }
 
--- Homework04/JSGuessTheNumber.java
+++ Homework04/JSGuessTheNumber.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class JSGuessTheNumber extends BaseGuessTheNumber {
 
+    private int maxTries;
     /**
      * Constructor to define minimum and maximum range of numbers for user to guess.
      * 
@@ -10,22 +11,41 @@ public class JSGuessTheNumber extends BaseGuessTheNumber {
      */
     public JSGuessTheNumber(int min, int max) {
         super(min, max);
+        maxTries = (max - min) + 1;
     }
 
+    /**
+     * calculates probability that user has to guess it correctly
+     * @return percentage user has chance to guess it correctly.
+     */
+    public double probability() {
+        int[] range = getRange();
+        return Double.parseDouble(String.format("%.1f", (1.0 / maxTries * 100)));
+    }
+
+    @Override
+    public boolean isMatch(int userGuess) {
+        boolean matched = (userGuess == getNumber2Guess()) ? true : false;
+        maxTries--;
+        return matched;
+    } 
+
     public static void main(String[] args) {
-        int[] range = new int[] {5,10};
-        JSGuessTheNumber jsGuessTheNumber = new JSGuessTheNumber(range[0],range[1]);
+        int[] range = new int[] {20,35};
+        JSGuessTheNumber jsGuessTheNumber = new JSGuessTheNumber(range[MIN],range[MAX]);
         Scanner scanner = new Scanner(System.in);
         boolean matched = false;
         do {
-            jsGuessTheNumber.newNumber2Guess();
             do {
-                System.out.println("Guess the number between "+range[0]+":"+range[1]);
+                System.out.println("Your chance of guessing it correctly " + jsGuessTheNumber.probability() + "%!");
+                System.out.println("Guess the number between "+range[MIN]+"-"+range[MAX]);
                 int userGuess = scanner.nextInt();
                 matched = jsGuessTheNumber.isMatch(userGuess);
             } while (!matched);
             System.out.println("Dude, you're awesome!");
             System.out.print("Do you want to play a new game? (yes/no)"); // prompt user to input yes or no
+            jsGuessTheNumber = new JSGuessTheNumber(range[MIN], range[MAX]);
+            jsGuessTheNumber.newNumber2Guess();
         } while (scanner.next().matches("(y|Y|yes|Yes|YES|Yeah|yeah|YEAH|yep|Yep|YEP|ok|OK)"));
         scanner.close();
     }
