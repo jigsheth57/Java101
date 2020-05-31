cac464792572de4c1384ab13b3435f498d14402e
Jig Sheth <jsheth@pivotal.io>
2020-05-31 07:29:32 -0500

    update

---
+++ Homework04/SmileyFace.java
@@ -0,0 +1,58 @@
+public class SmileyFace {
+
+    public static final int ROWS = 10;
+    public static final int COLUMNS = 21;
+
+    String[][] canvas = new String[ROWS][COLUMNS];
+
+    public void drawCanvas() {
+        for(int x=0; x<canvas.length; x++) {
+            for(int y=0; y<canvas[x].length; y++) {
+                canvas[x][y] = "-";
+            }
+        }
+    }
+
+    public void drawSmile() {
+        int row = canvas.length - 4;
+        canvas[row][2] = "$";
+        canvas[row][COLUMNS-3] = "$";
+        canvas[row+1][4] = "$";
+        canvas[row+1][COLUMNS-5] = "$";
+        int end = COLUMNS-7;
+        for(int y=6;y<=end;y++) {
+            canvas[row+2][y] = "$";
+        }
+    }
+
+    public void drawEyes() {
+        int row = canvas.length - 7;
+        canvas[row][7] = "0";
+        canvas[row][COLUMNS-8] = "0";
+    }
+
+    public void drawNose() {
+        int row = canvas.length - 5;
+        canvas[row][COLUMNS/2] = "?";
+    }
+
+    public String toString() {
+        StringBuffer sbuf = new StringBuffer();
+        for (int x = 0; x < canvas.length; x++) {
+            for (int y = 0; y < canvas[x].length; y++) {
+                sbuf.append(canvas[x][y]);
+            }
+            sbuf.append("\n");
+        }
+        return sbuf.toString();
+    }
+
+    public static void main(String[] args) {
+        SmileyFace smileyFace = new SmileyFace();
+        smileyFace.drawCanvas();
+        smileyFace.drawSmile();
+        smileyFace.drawEyes();
+        smileyFace.drawNose();
+        System.out.println(smileyFace);
+    }
+}
