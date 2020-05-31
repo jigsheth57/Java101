74c61a6eea05eea654bc98be8e44eb77a16faa2c
Jig Sheth <jsheth@pivotal.io>
2020-05-31 08:13:09 -0500

    update

--- Homework04/SmileyFace.java
+++ Homework04/SmileyFace.java
@@ -13,29 +13,48 @@ public class SmileyFace {
         }
     }
 
+    /**
+     * offset smile from nose (center point) by 1 row and span by 3 rows for big smile :D
+     */
     public void drawSmile() {
-        int center = ROWS/2;
-        int row = center + 1;
-        canvas[row][2] = "$";
-        canvas[row][COLUMNS-3] = "$";
-        canvas[row+1][4] = "$";
-        canvas[row+1][COLUMNS-5] = "$";
-        int end = COLUMNS-7;
-        for(int y=6;y<=end;y++) {
-            canvas[row+2][y] = "$";
+        int centerX = ROWS / 2;
+        int centerY = COLUMNS / 2;
+
+        int smileRow = centerX + 1;
+
+        // symmetrically to nose (8 column offset on both side from center)
+        canvas[smileRow][centerY + 8] = "$";
+        canvas[smileRow][centerY - 8] = "$";
+
+        // span smile to next row
+        canvas[smileRow+1][centerY + 6] = "$";
+        canvas[smileRow+1][centerY - 6] = "$";
+
+        // span smile to next row and fill the smile :D
+        int end = centerY + 4;
+        for (int y = centerY - 4; y <= end; y++) {
+            canvas[smileRow + 2][y] = "$";
         }
     }
 
+    /**
+     * Offset eyes from nose (center point). Use the symmetrical principle your art teacher taught you ;-)
+     */
     public void drawEyes() {
-        int row = canvas.length / 2 - 2;
-        int center = COLUMNS/2;
-        canvas[row][center+5] = "0";
-        canvas[row][center-5] = "0";
+        int centerX = ROWS/2;
+        int centerY = COLUMNS/2;
+        // two rows above nose
+        int eyeRow = centerX - 2;
+        // symmetrically to nose (5 column offset on both side from center)
+        canvas[eyeRow][centerY+5] = "0";
+        canvas[eyeRow][centerY-5] = "0";
     }
 
+    /**
+     * use center point to place nose.
+     */
     public void drawNose() {
-        int row = canvas.length/2;
-        canvas[row][COLUMNS/2] = "?";
+        canvas[ROWS/2][COLUMNS/2] = "?";
     }
 
     public String toString() {
@@ -52,9 +71,9 @@ public class SmileyFace {
     public static void main(String[] args) {
         SmileyFace smileyFace = new SmileyFace();
         smileyFace.drawCanvas();
-        smileyFace.drawSmile();
-        smileyFace.drawEyes();
         smileyFace.drawNose();
+        smileyFace.drawEyes();
+        smileyFace.drawSmile();
         System.out.println(smileyFace);
     }
 }
